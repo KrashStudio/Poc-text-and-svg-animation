2a487a46fca873790e6279e8ce2dffd8155e69b4
Joan <joan@outlook.fr>
2022-05-04 11:12:34 +0200

    :bug: : Remove this useless commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import PaperWithButton from "./components/PaperWithButton";
 import Rotate from "./components/Rotate";
 import Zoom from "./components/Zoom";
 
+// this is a test
+
 const App = () => {
 	const [fadeIsIn, setFadeIsIn] = React.useState(false);
 	const [fadeByLetterIsIn, setFadeByLetterIsIn] = React.useState(false);
